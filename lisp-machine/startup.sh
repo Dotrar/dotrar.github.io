687d3bb0701aaa39b900b29bcccfc6eceb71bab8
Dre Westcook <dre.westcook@octoenergy.com>
2021-11-17 16:43:56 +1100

    changes

--- lisp-machine/startup.sh
+++ lisp-machine/startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-tmux new-session -d
-tmux new-window
-tmux split-window -v -l 10 '~/fennel'
-tmux attach-session
+session="LispMachine"
+
+tmux new-session -d -s $session
+tmux rename-window -t 0 'lispmachine'
+tmux send-keys -t 'lispmachine' 'nvim' C-m
+tmux split-window -v -l 10
+tmux send-keys './fennel' C-m
+tmux attach -t $session:0
